efdaf3409b197471d1e7a7d9e34441eb4b5ee763
droplet <droplet127@outlook.com>
2025-06-25 17:32:35 +0800

    update

--- basic_usage.py
+++ basic_usage.py
@@ -10,9 +10,9 @@ cosyvoice = CosyVoice2('pretrained_models/CosyVoice2-0.5B', load_jit=False, load
 
 # NOTE if you want to reproduce the results on https://funaudiollm.github.io/cosyvoice2, please add text_frontend=False during inference
 # zero_shot usage
-prompt_speech_16k = load_wav('./asset/zero_shot_prompt.wav', 16000)
+prompt_speech_16k = load_wav('asset\cross_lingual_prompt.wav', 16000)
 
-for i, j in enumerate(cosyvoice.inference_zero_shot('收到好友从远方寄来的生日礼物，那份意外的惊喜与深深的祝福让我心中充满了甜蜜的快乐，笑容如花儿般绽放。',
-                                                    '希望你以后能够做的比我还好哟',
-                                                    prompt_speech_16k)):
+for i, j in enumerate(cosyvoice.inference_instruct2(tts_text='我国东北的小兴安岭，有数不清的树，几百里连成一片，就像绿色的海洋。',
+                                                    instruct_text='用伤心的语气说这句话',
+                                                    prompt_speech_16k=prompt_speech_16k)):
     torchaudio.save(f'tmp/{datetime.datetime.now().strftime("%Y%m%d-%H%M%S-bs")}.wav', j['tts_speech'], cosyvoice.sample_rate)
