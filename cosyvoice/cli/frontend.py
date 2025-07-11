9c6e9d87a75736dce266ed35e499c7e5902f5237
LeaF127 <1301868645@qq.com>
2025-07-11 11:07:37 +0800

    手动禁用ttsfrd

--- cosyvoice/cli/frontend.py
+++ cosyvoice/cli/frontend.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from functools import partial
+# import sys # 禁用 ttsfrd
+# sys.modules['ttsfrd'] = None
 from typing import Generator
 import json
 import onnxruntime
